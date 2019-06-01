7178b64e2fca890073b2ee3917ebdb63c224a62b
AhnJaeWook <dingchi@naver.com>
2019-06-01 13:59:06 +0900

    test

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    blog = Blog.objects.all()
+
+    return render(request, 'home.html', {'blog':blog})
