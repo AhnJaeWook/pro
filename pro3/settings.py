9967f2527360f35598102bb71100752e328dc03e
AhnJaeWook <dingchi@naver.com>
2019-06-01 13:45:27 +0900

    tset

--- pro3/settings.py
+++ pro3/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b=2&rvv3kvf7zf49b4#!@u)ix$^m$c*+n$5+kjq(r*6u=st16n'
-
+#SECRET_KEY = 'b=2&rvv3kvf7zf49b4#!@u)ix$^m$c*+n$5+kjq(r*6u=st16n'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
